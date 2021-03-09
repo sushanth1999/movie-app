495b8b988a558f4a204f52073a9424c5393e992c
sushanth <sushanthgupta9563@gmail.com>
2021-03-09 21:12:23 +0530

    Made structure of our app

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,27 @@
+import {data} from '../data.js'
+import Navbar from './Navbar'
+import MovieCard from './MovieCard'
 
 function App() {
   return (
     <div className="App">
-      Project Setup  
+      <Navbar>
+
+      </Navbar>
+      <div className='main'>
+        
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourite</div>
+        </div>
+        
+        <div className='list'>
+            {data.map(movie=>(
+               <MovieCard movie={movie} />
+            ))}
+
+        </div>
+      </div>
     </div>
   );
 }
